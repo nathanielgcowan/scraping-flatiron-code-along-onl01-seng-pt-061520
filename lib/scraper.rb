eef582b770d5cf8bac899f67ce93dce8b0ec2e63
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-17 13:10:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,24 +4,24 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def get_page
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    end
-  
+    Nokogiri::HTML(open("http://austinswingsyndicate.org/"))
+  end
+
   def get_courses
-    self.get_page.css(".post")
+    self.get_page.css("div.slide-content")
   end
-  
+
   def make_courses
-    self.get_courses.each do |post|
+    self.get_courses.each do |thing|
       course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css("date").text
-      course.description = post.css("p").text
+      course.title = thing.css("h3").text
+      course.schedule = thing.css("time.slide-meta-time updated").text
+      course.description = thing.css("div.slide-entry-excerpt entry-content").text.strip
+    end
   end
-end
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -32,8 +32,5 @@ end
       end
     end
   end
-  
+
 end
-
-
-
